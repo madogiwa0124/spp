a78ca843b15cbc618c2195c6ae4850bce15fac55
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2018-08-09 00:16:35 +0900

    add spp_bench with spec

--- lib/spp.rb
+++ lib/spp.rb
@@ -1,5 +1,6 @@
 require "spp/version"
 require "pp"
+require 'date'
 
 module Spp
   DEFAULT_START_STR = 'START'
@@ -14,6 +15,19 @@ module Spp
       puts "#{line_str} #{start_str} #{line_str}"
       pp message
       puts "#{line_str} #{end_str} #{line_str}"
+    end
+
+    def spp_bench(start_str=nil, end_str=nil, line_str=nil)
+      unless block_given?
+        puts "[Error] No processing is given. Please give block argument."
+        return false
+      end
+      start_str ||= DEFAULT_START_STR
+      end_str   ||= DEFAULT_END_STR
+      line_str  ||= DEFAULT_LINE_STR
+      puts "#{line_str} #{start_str}(#{Time.now}) #{line_str}"
+      pp yield
+      puts "#{line_str} #{end_str}(#{Time.now}) #{line_str}"
     end
   end
 end
--- spec/spp_spec.rb
+++ spec/spp_spec.rb
@@ -1,4 +1,5 @@
 require "stringio"
+require "timecop"
 
 RSpec.describe Spp do
   it "has a version number" do
@@ -28,6 +29,49 @@ RSpec.describe Spp do
           ⚡⚡⚡ end ⚡⚡⚡
         EOS
         expect($stdout.string).to eq text
+      end
+    end
+  end
+
+  describe ".spp_bench" do
+    before do 
+      $stdout = StringIO.new
+      Timecop.freeze(Time.now)
+    end
+
+    context 'nothing argument' do
+      it 'output with default decoration' do
+        Spp::spp_bench do
+          'hoge'
+        end
+        text = <<~EOS
+          ========== START(#{Time.now}) ==========
+          "hoge"
+          ========== E N D(#{Time.now}) ==========
+        EOS
+        expect($stdout.string).to eq text
+      end
+    end
+
+    context 'with argument' do
+      it 'output with argumrnt decoration' do
+        Spp::spp_bench('start', 'end', '⚡' * 3) do
+          'hoge'
+        end
+        text = <<~EOS
+          ⚡⚡⚡ start(#{Time.now}) ⚡⚡⚡
+          "hoge"
+          ⚡⚡⚡ end(#{Time.now}) ⚡⚡⚡
+        EOS
+        expect($stdout.string).to eq text
+      end
+    end
+
+    context 'nothing block' do
+      it 'output error message' do
+        Spp::spp_bench
+        text = "No processing is given. Please give block argument."
+        expect($stdout.string).to be_include(text)
       end
     end
   end
